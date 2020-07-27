fc78d7f8aa6cfd8955ea1dac3dba3dfc6375c084
Colton Lathrop <colathro@microsoft.com>
2020-07-27 11:36:04 -0500

    initial commit

---
+++ ClientApp/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Route } from "react-router";
+
+import "./App.css";
+
+export default class App extends Component {
+  static displayName = App.name;
+
+  render() {
+    return <div>test</div>;
+  }
+}
---
+++ Controllers/BaseController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ndso_bowling.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BaseController : ControllerBase
+    {
+
+        private readonly ILogger<BaseController> _logger;
+
+        public BaseController(ILogger<BaseController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Working!");
+        }
+    }
+}
